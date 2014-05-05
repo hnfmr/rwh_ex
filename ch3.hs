98573e0707b9b24d5e4cd072536e22970c3b23a2
Wei Qi <qiwei.uk@gmail.com>
2014-05-05 23:23:29 +0800

    ch3 wip

--- ch3.hs
+++ ch3.hs
@@ -25,47 +25,79 @@ height :: Tree a -> Int
 height Empty = 0
 height (Node a b c) = 1 + max (height b) (height c)
 
-data Point = Point { x :: Int,
-                     y :: Int }
+data Point = Point { x :: Double,
+                     y :: Double }
              deriving (Show)
 
-data Line = Line { a :: Int,
-                   b :: Int,
-                   c :: Int  }
-            deriving (Show)
+xdiff :: Point -> Point -> Double
+xdiff (Point x0 y0) (Point x1 y1) = abs (x0 - x1)
+
+ydiff :: Point -> Point -> Double
+ydiff (Point x0 y0) (Point x1 y1) = abs (y0 - y1)
 
 data Direction = LeftT | RightT | StraightT deriving (Show)
+
+data Line = Line { a :: Double,
+                   b :: Double,
+                   c :: Double  }
+            deriving (Show)
+
+data PointDirection = LeftL | RightL | SameL
+
+gradient :: Point -> Point -> Double
+gradient (Point x0 y0) (Point x1 y1) = (y1-y0)/(x1-x0)
+
+intercept :: Point -> Point -> Double
+intercept a b =
+  let (Point x0 y0) = a
+  in x0 - (gradient a b)*x0
+
+gradient' :: Line -> Double
+gradient' (Line a b c) = (-a)/b
+
+intercept' :: Line -> Double
+intercept' (Line _ b c) = (-c)/b
 
 line :: Point -> Point -> Line
 line (Point a1 b1) (Point a2 b2) = let a = b1 - b2
                                        b = a2 - a1
                                        c = a1*b2 - a2*b1
                                    in (Line a b c)
+
+pointDirection :: Point -> Line -> PointDirection
+pointDirection (Point x0 y0) (Line a b c)
+  | (a*x0+b*y0+c < 0) = LeftL
+  | (a*x0+b*y0+c > 0) = RightL
+  | otherwise = SameL
+
 distance :: Point -> Point -> Double
 distance (Point x0 y0) (Point x1 y1) =
   let ydiff = abs (y1 - y0)
       xdiff = abs (x1 - x0)
-  in sqrt $ fromIntegral (ydiff^2 + xdiff^2)
+  in sqrt $ (ydiff^2 + xdiff^2)
 
 distanceToLine :: Point -> Line -> Double
 distanceToLine (Point x y) (Line a b c) = let numerator = abs (a*x + b*y + c)
                                               denumerator = let operand = a^2 + b^2
-                                                            in sqrt (fromIntegral operand)
+                                                            in sqrt operand
                                           in
-                                          let distance' = (fromIntegral numerator) / denumerator
+                                          let distance' = numerator / denumerator
                                           in abs distance'
 
-xdiff :: Point -> Point -> Int
-xdiff (Point x0 y0) (Point x1 y1) = abs (x0 - x1)
-
-ydiff :: Point -> Point -> Int
-ydiff (Point x0 y0) (Point x1 y1) = abs (y0 - y1)
 
 radianToDegree :: Double -> Double
 radianToDegree r = 360 * r / (2*pi)
 
 abAngle :: Point -> Point -> Double
-abAngle a b = atan $ (fromIntegral $ ydiff a b) / (fromIntegral $ xdiff a b)
+abAngle a b = atan $ ydiff a b / xdiff a b
+
+turn :: Point -> Point -> Point -> Direction
+turn a b c
+  | prod > 0 = LeftT
+  | prod < 0 = RightT
+  | otherwise = StraightT
+  where prod = (gradient a c)/(gradient a b)
+
 {-
 angle :: Point -> Point -> Point -> Double
 angle a b c = let abDist = distance a b
