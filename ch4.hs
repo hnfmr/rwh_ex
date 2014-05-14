aad73ff19ec419aa74495013fe2eb72da00a4b7c
Wei Qi <qiwei.uk@gmail.com>
2014-05-14 11:46:30 -0700

    WIP

--- ch4.hs
+++ ch4.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 safeHead :: [a] -> Maybe a
 safeHead [] = Nothing
 safeHead (x:xs) = Just x
@@ -26,3 +28,37 @@ splitWith f xs = let fs = filter f xs
 transposeText :: String -> String
 transposeText input = let (l:r:[]) = lines input
                       in concat $ zipWith (\a b -> a:b:'\n':[]) l r
+
+asInt :: String -> Int
+asInt [] = 0
+asInt (c:cs) = if c=='-'
+               then foldl (\a c -> a*10 - digitToInt c) 0 cs
+               else foldl (\a c -> a*10 + digitToInt c) 0 (c:cs)
+               
+type ErrorMessage = String
+asInt_either :: String -> Either ErrorMessage Int
+asInt_either [] = Left "Contains no digit"
+asInt_either ('-':cs) = case asInt_either cs of
+                          Left err -> Left err
+                          Right val -> Right (-val)
+asInt_either cs = foldl step (Right 0) cs
+  where step (Right a) c | isDigit c = Right (a*10 + digitToInt c)
+                         | otherwise = Left ([c] ++ " is not a digit!")
+        step (Left err) _ = Left err
+        
+groupBy' :: (a -> a -> Bool) -> [a] -> [[a]]
+groupBy' f xs = step [] xs
+  where step acc [] = acc
+        step acc (x':xs') =
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
