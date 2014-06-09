e0672747791b9e6fb8887365218e9bb4d945327d
Wei Qi <qiwei.uk@gmail.com>
2014-06-10 00:20:01 +0800

    ch10

--- ch10/Parse.hs
+++ ch10/Parse.hs
@@ -2,6 +2,9 @@
 import Data.Int (Int64)
 import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Data.ByteString.Lazy as L
+import Data.Word (Word8)
+import Control.Applicative
+import Data.Char (chr)
 
 data ParseState = ParseState {
        string :: L.ByteString
@@ -24,7 +27,7 @@ parse parser initState
 modifyOffset :: ParseState -> Int64 -> ParseState
 modifyOffset initState newOffset =
     initState { offset = newOffset }
-    
+
 parseByte :: Parse Word8
 parseByte =
     getState ==> \initState ->
@@ -37,3 +40,46 @@ parseByte =
         where newState = initState { string = remainder,
                                      offset = newOffset }
               newOffset = offset initState + 1
+
+getState :: Parse ParseState
+getState = Parse (\s -> Right (s, s))
+
+putState :: ParseState -> Parse ()
+putState s = Parse (\_ -> Right ((), s))
+
+(==>) :: Parse a -> (a -> Parse b) -> Parse b
+firstParser ==> secondParser   = Parse chainedParser
+  where chainedParser initState =
+          case runParse firstParser initState of
+            Left errMessage ->
+                Left errMessage
+            Right (firstResult, newState) ->
+                runParse (secondParser firstResult) newState
+
+bail :: String -> Parse a
+bail err = Parse $ \s -> Left $
+           "byte offset " ++ show (offset s) ++ ": " ++ err
+
+instance Functor Parse where
+    fmap f parser = parser ==> \result ->
+                    identity (f result)
+
+w2c :: Word8 -> Char
+w2c = chr . fromIntegral
+
+parseChar :: Parse Char
+parseChar = w2c <$> parseByte
+
+peekByte :: Parse (Maybe Word8)
+peekByte = (fmap fst . L.uncons . string) <$> getState
+
+peekChar :: Parse (Maybe Char)
+peekChar = fmap w2c <$> peekByte
+
+
+parseWhile :: (Word8 -> Bool) -> Parse [Word8]
+parseWhile p = (fmap p <$> peekByte) ==> \mp ->
+               if mp == Just True
+               then parseByte ==> \b ->
+                    (b:) <$> parseWhile p
+               else identity []
