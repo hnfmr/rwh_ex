0b48c4f40aebf7f5410ad58fd7530e4d63f31438
Wei Qi <qiwei.uk@gmail.com>
2014-06-24 10:29:15 +0800

    ch15 wip

--- ch15/Supply.hs
+++ ch15/Supply.hs
@@ -1,5 +1,6 @@
 -- file: ch15/Supply.hs
 
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Supply
     (
       Supply
@@ -10,7 +11,20 @@ module Supply
 import Control.Monad.State
 
 newtype Supply s a = S (State [s] a)
+  deriving (Monad)
 
 runSupply :: Supply s a -> [s] -> (a, [s])
+runSupply (S m) xs = runState m xs
 
 next :: Supply s (Maybe s)
+next = S $ do st <- get
+              case st of
+                [] -> return Nothing
+                (x:xs) -> do put xs
+                             return (Just x)
+
+showTwo :: (Show s) => Supply s String
+showTwo = do
+  a <- next
+  b <- next
+  return (show "a: " ++ show a ++ ", b: " ++ show b)
