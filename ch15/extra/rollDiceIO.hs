2d6100cb374871610dc38aa8f81a6fe6f5b99188
Wei Qi <qiwei.uk@gmail.com>
2014-06-30 18:29:55 -0700

    wip

--- ch15/extra/rollDiceIO.hs
+++ ch15/extra/rollDiceIO.hs
@@ -7,10 +7,25 @@ rollDiceIO = liftM2 (,) (randomRIO (1,6)) (randomRIO (1,6))
 rollNDiceIO :: Int -> IO [Int]
 rollNDiceIO n = mapM (\_ -> randomIO) [1..n]
 
+clumsyRollDice :: (Int, Int)
+clumsyRollDice = (n, m)
+    where
+    (n, g) = randomR (1, 6) (mkStdGen 0)
+    (m, _) = randomR (1, 6) g
+
+rollDice :: StdGen -> ((Int, Int), StdGen)
+rollDice gen = ((a, b), g')
+    where
+    (a, g) = randomR (1, 6) gen
+    (b, g') = randomR (1, 6) g'
+  
+--    let (a, g) = randomR (1, 6) gen
+--      in let (b, g') = randomR (1, 6) g
+--        in ((a, b), g')
 --main = do
 --  (a, b) <- rollDiceIO
 --  putStrLn (show a ++ ", " ++ show b)
 
 main = do
-    xs <- rollNDiceIO 10
-    putStrLn (show xs)
+    let ((a, b), _) = rollDice (mkStdGen 0)
+      in putStrLn (show a ++ ", " ++ show b)
