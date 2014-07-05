43e71c0e8c1e4f525c7dc2ee0e727ed754b11e92
Wei Qi <qiwei.uk@gmail.com>
2014-07-04 17:26:33 -0700

    wip

---
+++ ch15/extra/diceState.hs
@@ -0,0 +1,15 @@
+﻿-- file: ch15/extra/diceState.hs
+import Control.Monad
+import Control.Monad.Trans.State
+import System.Random
+
+type GeneratorState = State StdGen
+
+rollDie :: GeneratorState Int
+rollDie = do generator <- get
+             let (value, newGenerator) = randomR (1,6) generator
+             put newGenerator
+             return value
+
+rollDice :: GeneratorState (Int, Int)
+rollDice = liftM2 (,) rollDie rollDie
