b035ee2c2e14a0a788b61091ddf89b277e744a4b
Wei Qi <qiwei.uk@gmail.com>
2014-08-05 12:51:30 -0700

    ch19 wip

--- ch17/Regex.hs
+++ ch17/Regex.hs
@@ -6,7 +6,7 @@ module Regex where
 import Foreign
 import Foreign.C.Types
 
-#include <pcre.h>
+#include "pcre.h"
 
 -- | A type for PCRE compile-time options. These are newtyped CInts,
 -- which can be bitwise-or'd together, using '(Data.Bits..|.)'
