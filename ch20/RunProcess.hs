a3f1ebea85a40e95c810873fec4573ffd9b50bde
Wei Qi <qiwei.uk@gmail.com>
2014-08-11 09:03:48 +0800

    wip

--- ch20/RunProcess.hs
+++ ch20/RunProcess.hs
@@ -1,12 +1,13 @@
 -- file: ch20/RunProcess.hs
-{-# OPTIONS_GHC -fglasgow-exts #-}
+-- {-# OPTIONS_GHC -fglasgow-exts #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module RunProcess where
 
 import System.Process
 import Control.Concurrent
 import Control.Concurrent.MVar
-import Control.OldException(evaluate)
+-- import Control.OldException(evaluate)
 import System.Posix.Directory
 import System.Directory(setCurrentDirectory)
 
@@ -54,7 +55,7 @@ instance CommandLike SysCommand where
            
            -- Now, grab the closed FDs list and fork the child.
            childPID <- withMVar closefds (\fds ->
-                         forkProcess (child fds stdinread stdoutwrite)
+                         forkProcess (child fds stdinread stdoutwrite))
                          
            -- Now, on the parent, close the client-side FDs.
            closeFd stdinread
@@ -71,28 +72,57 @@ instance CommandLike SysCommand where
            -- Set up the function to call when ready to wait for the child to
            -- exit
            let waitfunc =
-               do status <- getProcessStatus True False childPID
-                   case status of
-                       Nothing -> fail $ "Error: Nothing from getProcessStatus"
-                       Just ps -> do removeCloseFDs closefds
-                                         [stdinwrite, stdoutread]
-                                     return ps
-           return $ CommandResult {cmdOutput = hGetContents sdouthdl,
-                                  getExitStatus = waitfunc}
+                 do status <- getProcessStatus True False childPID
+                    case status of
+                        Nothing -> fail $ "Error: Nothing from getProcessStatus"
+                        Just ps -> do removeCloseFDs closefds
+                                          [stdinwrite, stdoutread]
+                                      return ps
+           return $ CommandResult {cmdOutput = hGetContents stdouthdl,
+                                   getExitStatus = waitfunc}
+        -- Define what happens in the child process
+        where child closefds stdinread stdoutwrite =
+                do -- Copy our pipes over the regular stdin/stdout FDs
+                  dupTo stdinread stdInput
+                  dupTo stdoutwrite stdOutput
 
+                  -- Now close the original pipe FDs
+                  closeFd stdinread
+                  closeFd stdoutwrite
 
+                  -- Close all the open FDs we inherited from the parent
+                  mapM_ (\fd -> catch (closeFd fd) (\_ -> return ())) closefds
 
+                  -- Start the program
+                  executeFile cmd True args Nothing
 
+{- | An instance of 'CommandLike' for an external command. The String is passed
+ to a shell for evaluation and invocation. -}
+instance CommandLike String where
+    invoke cmd closefds input =
+        do -- Use the shell given by the environment variables SHELL,
+           -- if any. Otherwise, use /bin/sh
+           esh <- getEnv "SHELL"
+           let sh = case esh of
+                        Nothing -> "/bin/sh"
+                        Just x -> x
+           invoke (sh, ["-c", cmd]) closefds input
 
+-- Add FDs to the list of FDs that must be closed post-fork in a child
+addCloseFDs :: CloseFDs -> [Fd] -> IO ()
+addCloseFDs closefds newfds =
+    modifyMVar_ closefds (\oldfds -> return $ oldfds ++ newfds)
 
+removeCloseFDs :: CloseFDs -> [Fd] -> IO ()
+removeCloseFDs closefds removethem =
+    modifyMVar_ closefds (\fdlist -> return $ procfdlist fdlist removethem)
 
+    where
+    procfdlist fdlist [] = fdlist
+    procfdlist fdlist (x:xs) = procfdlist (removefd fdlist x) xs
 
-
-
-
-
-
-
-
-
-          
+    -- We want to remove only the first occurance of any given fd
+    removefd [] _ = []
+    removefd (x:xs) fd
+        | fd == x = xs
+        | otherwise = x : removefd xs fd
